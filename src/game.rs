050a4998da2714d4d4b4eae9cd5a04f2234c474f
ihor-tarasov <mov.ax.38@gmail.com>
2023-11-19 19:08:52 +0200

    Initial commit

---
+++ src/game.rs
@@ -0,0 +1,158 @@
+use std::f32::consts::PI;
+
+use macroquad::prelude::*;
+
+use crate::{Asteroid, Bullet, Explosions, Ship};
+
+const DISPLAY_SCALE: f32 = 1000.0;
+
+pub struct Game {
+    ship: Ship,
+    bullets: Vec<Bullet>,
+    explosions: Explosions,
+    asteroids: Vec<Asteroid>,
+    world_camera: Camera2D,
+}
+
+impl Game {
+    pub fn new() -> Self {
+        Self {
+            ship: Ship::new(),
+            bullets: Vec::new(),
+            explosions: Explosions::new(),
+            asteroids: Vec::new(),
+            world_camera: Camera2D {
+                rotation: 0.0,
+                zoom: vec2(
+                    1.0 / DISPLAY_SCALE,
+                    (1.0 / DISPLAY_SCALE) * (screen_width() / screen_height()),
+                ),
+                target: vec2(0.0, 0.0),
+                offset: vec2(0.0, 0.0),
+                render_target: None,
+                viewport: None,
+            },
+        }
+    }
+
+    fn generate_asteroid(&mut self) {
+        if rand::gen_range(0, 100) == 0 {
+            let angle = rand::gen_range(0.0, PI * 2.0);
+            let position = Vec2::from_angle(angle) * DISPLAY_SCALE * 2.0;
+            let velocity = (self.ship.position() - position).normalize();
+            self.asteroids.push(Asteroid::new(position, velocity));
+        }
+    }
+
+    fn update_game_objects(&mut self, dt: f32) {
+        self.ship.update(dt);
+        self.bullets.iter_mut().for_each(|bullet| bullet.update(dt));
+        self.asteroids
+            .iter_mut()
+            .for_each(|asteroid| asteroid.update(dt));
+        self.explosions.update(dt);
+    }
+
+    fn process_collisions(&mut self) {
+        let mut new_asteroids = Vec::new();
+        for bullet in &mut self.bullets {
+            for asteroid in &mut self.asteroids {
+                if bullet.collision(asteroid.position(), asteroid.radius()) {
+                    bullet.destroy();
+                    asteroid.destroy(&mut new_asteroids);
+                    self.explosions
+                        .explode(asteroid.position(), asteroid.size());
+                }
+            }
+        }
+
+        let count = self.asteroids.len();
+        for i in 0..count {
+            for j in 0..count {
+                if i != j {
+                    if self.asteroids[i].collision(&self.asteroids[j]) {
+                        self.asteroids[i].destroy(&mut new_asteroids);
+                        self.asteroids[j].destroy(&mut new_asteroids);
+                        self.explosions
+                            .explode(self.asteroids[i].position(), self.asteroids[i].size());
+                        self.explosions
+                            .explode(self.asteroids[j].position(), self.asteroids[j].size());
+                    }
+                }
+            }
+        }
+
+        self.asteroids.extend(new_asteroids);
+    }
+
+    fn remove_objects(&mut self) {
+        self.bullets.retain(|bullet| bullet.alive());
+        self.asteroids.retain(|asteroid| asteroid.alive());
+    }
+
+    fn update_camera(&mut self) {
+        self.world_camera.zoom = vec2(
+            1.0 / (DISPLAY_SCALE + self.ship.speed()),
+            (1.0 / (DISPLAY_SCALE + self.ship.speed())) * (screen_width() / screen_height()),
+        );
+        self.world_camera.target = self.ship.position();
+    }
+
+    pub fn update(&mut self) {
+        let dt = get_frame_time();
+
+        self.generate_asteroid();
+
+        if is_key_pressed(KeyCode::M) {
+            self.bullets.push(self.ship.fire());
+        }
+
+        self.update_game_objects(dt);
+        self.process_collisions();
+        self.remove_objects();
+        self.update_camera();
+    }
+
+    pub fn draw(&self) {
+        clear_background(LIGHTGRAY);
+        set_camera(&self.world_camera);
+        self.explosions.draw();
+        self.ship.draw();
+        self.bullets.iter().for_each(|bullet| bullet.draw());
+        self.asteroids.iter().for_each(|asteroid| asteroid.draw());
+        set_default_camera();
+
+        draw_text(
+            format!("FPS: {}", get_fps()).as_str(),
+            0.0,
+            20.0,
+            20.0,
+            BLACK,
+        );
+        draw_text(
+            format!("Bullets: {}", self.bullets.len()).as_str(),
+            0.0,
+            40.0,
+            20.0,
+            BLACK,
+        );
+        draw_text(
+            format!("Asteroids: {}", self.asteroids.len()).as_str(),
+            0.0,
+            60.0,
+            20.0,
+            BLACK,
+        );
+        draw_text(
+            format!(
+                "Particles: {}",
+                self.explosions.particles_count() + self.ship.particles_count()
+            )
+            .as_str(),
+            0.0,
+            80.0,
+            20.0,
+            BLACK,
+        );
+    }
+}
---
+++ src/ship.rs
@@ -0,0 +1,155 @@
+use std::f32::consts::PI;
+
+use macroquad::prelude::*;
+
+use crate::{Bullet, Particle};
+
+const HEIGHT: f32 = 25.0;
+const SHOLDER: f32 = 22.0;
+const MAX_ROTATION_SPEED: f32 = 7.0;
+const MOVE_ACCELERATION: f32 = 300.0;
+const ROTATION_ACCELERATION: f32 = 14.0;
+const MAX_SPEED: f32 = 300.0;
+const MOVE_FRICTION: f32 = 100.0;
+const ROTATION_FRICTION: f32 = 10.0;
+const PARTICLE_ANGLE_DIFF: f32 = PI / 4.0;
+
+pub struct Ship {
+    position: Vec2,
+    speed: f32,
+    angle: f32,
+    rotation_speed: f32,
+    particles: Vec<Particle>,
+}
+
+impl Ship {
+    pub fn new() -> Self {
+        Self {
+            position: vec2(0.0, 0.0),
+            speed: 0.0,
+            angle: -PI / 2.0,
+            rotation_speed: 0.0,
+            particles: vec![],
+        }
+    }
+
+    fn handle_rotation_input(&mut self, dt: f32) {
+        let mut pressed = false;
+        if is_key_down(KeyCode::A) {
+            pressed = true;
+            self.rotation_speed -= ROTATION_ACCELERATION * dt;
+            if self.rotation_speed < -MAX_ROTATION_SPEED {
+                self.rotation_speed = -MAX_ROTATION_SPEED;
+            }
+        }
+        if is_key_down(KeyCode::D) {
+            pressed = true;
+            self.rotation_speed += ROTATION_ACCELERATION * dt;
+            if self.rotation_speed > MAX_ROTATION_SPEED {
+                self.rotation_speed = MAX_ROTATION_SPEED;
+            }
+        }
+        if !pressed {
+            if self.rotation_speed > 0.0 {
+                self.rotation_speed -= ROTATION_FRICTION * dt;
+            } else {
+                self.rotation_speed += ROTATION_FRICTION * dt;
+            }
+        }
+    }
+
+    fn handle_move_input(&mut self, dt: f32) -> bool {
+        let acceleration;
+
+        if is_key_down(KeyCode::W) {
+            acceleration = MOVE_ACCELERATION;
+        } else if is_key_down(KeyCode::S) {
+            acceleration = -MOVE_ACCELERATION;
+        } else {
+            acceleration = -MOVE_FRICTION;
+        }
+
+        self.speed += acceleration * dt;
+
+        if self.speed > MAX_SPEED {
+            self.speed = MAX_SPEED;
+        } else if self.speed < 0.0 {
+            self.speed = 0.0;
+        }
+
+        acceleration > 0.0
+    }
+
+    fn apply_rotation_speed(&mut self, dt: f32) {
+        self.angle += self.rotation_speed * dt;
+
+        while self.angle > PI * 2.0 {
+            self.angle -= PI * 2.0;
+        }
+    }
+
+    fn appy_move(&mut self, dt: f32) {
+        self.position += Vec2::from_angle(self.angle) * self.speed * dt;
+    }
+
+    fn update_particles(&mut self, dt: f32, accelerated: bool) {
+        self.particles.retain(|particle| particle.is_alive());
+        self.particles
+            .iter_mut()
+            .for_each(|particle| particle.update(dt));
+
+        if accelerated {
+            let particle_start = self.position + Vec2::from_angle(self.angle + PI) * HEIGHT / 2.0;
+            let particle_angle =
+                self.angle + PI + rand::gen_range(-PARTICLE_ANGLE_DIFF, PARTICLE_ANGLE_DIFF);
+            self.particles.push(Particle::new(
+                particle_start,
+                particle_angle,
+                0.1 + rand::gen_range(0.0, 0.1),
+            ));
+        }
+    }
+
+    pub fn update(&mut self, dt: f32) {
+        self.handle_rotation_input(dt);
+        self.apply_rotation_speed(dt);
+        let accelerated = self.handle_move_input(dt);
+        self.appy_move(dt);
+        self.update_particles(dt, accelerated);
+    }
+
+    pub fn draw(&self) {
+        let left_sholder_angle = self.angle + PI / 2.0;
+        let direction = Vec2::from_angle(self.angle) * HEIGHT / 2.0;
+        let left_sholder_direction = Vec2::from_angle(left_sholder_angle) * SHOLDER / 2.0;
+
+        draw_triangle_lines(
+            self.position + direction,
+            self.position - direction + left_sholder_direction,
+            self.position - direction - left_sholder_direction,
+            2.0,
+            BLACK,
+        );
+
+        self.particles.iter().for_each(|particle| particle.draw());
+    }
+
+    pub fn fire(&self) -> Bullet {
+        Bullet::new(
+            self.position + Vec2::from_angle(self.angle) * HEIGHT / 2.0,
+            self.angle,
+        )
+    }
+
+    pub fn position(&self) -> Vec2 {
+        self.position
+    }
+
+    pub fn speed(&self) -> f32 {
+        self.speed
+    }
+
+    pub fn particles_count(&self) -> usize {
+        self.particles.len()
+    }
+}
